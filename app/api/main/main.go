c27db56f34233a9cd134690ad580eca3fe7593d1
pribadi1st <pribadiridwan@rocketmail.com>
2025-05-24 20:22:44 +0200

    feat: add basic server

---
+++ app/api/main/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/subosito/gotenv"
+)
+
+var (
+	srv    *server.Server
+	router *gin.Engine
+	ctx    context.Context
+)
+
+func initServer(ctx context.Context) {
+	gotenv.Load()
+}
+
+func main() {
+	ctx = context.Background()
+
+	initServer(ctx)
+
+	httpServer := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	// Graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	<-stop // Wait for interrupt signal
+
+	// Create a context with a timeout for the shutdown
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := httpServer.Shutdown(ctx); err != nil {
+		panic(err)
+	}
+}
